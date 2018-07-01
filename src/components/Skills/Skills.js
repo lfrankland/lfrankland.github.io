7eb4b6c290b81fe5e5f33e204783cf411ff04d7c
Louis Frankland <louis@frank.land>
2018-07-01 10:44:08 +0100

    Standardise skill logos

--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -3,6 +3,9 @@ import styles from './Skills.module.css';
 import html5 from './html5.svg';
 import css3 from './css3.svg';
 import sass from './sass.svg';
+import axure from './axure.svg';
+import illustrator from './illustrator.svg';
+import photoshop from './photoshop.svg';
 
 const Skills = () => (
 	<div className={styles.skills}>
@@ -11,6 +14,9 @@ const Skills = () => (
 			<li className={styles.skill}><img src={html5} alt="HTML5 Logo" /></li>			
 			<li className={styles.skill}><img src={css3} alt="CSS3 Logo" /></li>
 			<li className={styles.skill}><img src={sass} alt="Sass Logo" /></li>	
+			<li className={styles.skill}><img src={axure} alt="Axure RP Logo" /></li>			
+			<li className={styles.skill}><img src={illustrator} alt="Illustrator Logo" /></li>
+			<li className={styles.skill}><img src={photoshop} alt="Photoshop Logo" /></li>	
 		</ul>
 	</div>
 )
