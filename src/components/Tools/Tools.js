977868186c1cb62fb690117d1f90ffc52bc1cb13
Louis Frankland <louis@frank.land>
2019-10-27 13:54:01 +0000

    moved assets to static folder

--- src/components/Tools/Tools.js
+++ src/components/Tools/Tools.js
@@ -1,22 +1,22 @@
 import React from 'react'
 import styles from './Tools.module.css';
-import html5 from './html5.svg';
-import css3 from './css3.svg';
-import sass from './sass.svg';
-import axure from './axure.svg';
-import illustrator from './illustrator.svg';
-import photoshop from './photoshop.svg';
+//import html5 from './logos/html5.svg';
+//import css3 from './logos/css3.svg';
+//import sass from './logos/sass.svg';
+//import axure from './logos/axure.svg';
+//import illustrator from './logos/illustrator.svg';
+//import photoshop from './logos/photoshop.svg';
 
 const Tools = () => (
 	<div className={styles.tools}>
 		<h2 className={styles.title}>Tools</h2>
 		<ul className={styles.list}>
-			<li className={styles.tool}><img src={html5} alt="HTML5 Logo" /></li>			
-			<li className={styles.tool}><img src={css3} alt="CSS3 Logo" /></li>
-			<li className={styles.tool}><img src={sass} alt="Sass Logo" /></li>	
-			<li className={styles.tool}><img src={axure} alt="Axure RP Logo" /></li>			
-			<li className={styles.tool}><img src={illustrator} alt="Illustrator Logo" /></li>
-			<li className={styles.tool}><img src={photoshop} alt="Photoshop Logo" /></li>	
+			<li className={styles.tool}><img src={'/logos/html5.svg'} alt="HTML5 Logo" /></li>			
+			<li className={styles.tool}><img src={'/logos/css3.svg'} alt="CSS3 Logo" /></li>
+			<li className={styles.tool}><img src={'/logos/sass.svg'} alt="Sass Logo" /></li>	
+			<li className={styles.tool}><img src={'/logos/axure.svg'} alt="Axure RP Logo" /></li>			
+			<li className={styles.tool}><img src={'/logos/illustrator.svg'} alt="Illustrator Logo" /></li>
+			<li className={styles.tool}><img src={'/logos/photoshop.svg'} alt="Photoshop Logo" /></li>	
 		</ul>
 	</div>
 )
